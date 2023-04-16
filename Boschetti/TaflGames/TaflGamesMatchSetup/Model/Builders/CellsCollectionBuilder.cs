16f556fe858b4831f5714332079c42c12f5c5971
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-04-16 13:10:30 +0200

    feat: add cells collection builder

---
+++ Boschetti/TaflGames/TaflGamesMatchSetup/Model/Builders/CellsCollectionBuilder.cs
@@ -0,0 +1,47 @@
+﻿using Cells;
+using Common;
+
+namespace Builders
+{
+    /// <summary>
+    /// This class implements a builder to create a collection of cells.
+    /// </summary>
+	public class CellsCollectionBuilder : ICellsCollectionBuilder
+	{
+		private readonly IDictionary<IPosition, ICell> _cells;
+		private int _boardSize;
+
+        /// <summary>
+        /// Creates a new builder for a collection of cells.
+        /// </summary>
+		public CellsCollectionBuilder() => _cells = new Dictionary<IPosition, ICell>();
+
+        public void AddBoardSize(int boardSize) => _boardSize = boardSize;
+
+        public void AddThrone(IPosition position) => _cells.Add(position, new Throne());
+
+        public void AddExits(ISet<IPosition> positions)
+        {
+            positions.ToList().ForEach(pos => _cells.Add(pos, new Exit()));
+        }
+
+        public void AddSliders(ISet<IPosition> positions)
+        {
+            positions.ToList().ForEach(pos => _cells.Add(pos, new Slider()));
+        }
+
+        public void AddBasicCells()
+        {
+            foreach (int row in Enumerable.Range(0, _boardSize))
+            {
+                foreach (int column in Enumerable.Range(0, _boardSize))
+                {
+                    _cells.Add(new Position(row, column), new ClassicCell());
+                }
+            }
+        }
+
+        public IDictionary<IPosition, ICell> Build() => new Dictionary<IPosition, ICell>(_cells);
+
+    }
+}
---
+++ Boschetti/TaflGames/TaflGamesMatchSetup/Model/Builders/ICellsCollectionBuilder.cs
@@ -0,0 +1,44 @@
+﻿using Cells;
+using Common;
+
+namespace Builders
+{
+	/// <summary>
+	/// This interface allows to intercat with a builder that creates
+	/// a collection of cells.
+	/// </summary>
+	public interface ICellsCollectionBuilder
+	{
+        /// <summary>
+        /// Registers the size of the board.
+        /// </summary>
+        /// <param name="boardSize">the size of the board</param>
+        void AddBoardSize(int boardSize);
+
+		/// <summary>
+		/// Adds the throne cell to the cells collection being built.
+		/// </summary>
+		/// <param name="position">the position where the throne must be placed</param>
+		void AddThrone(IPosition position);
+
+        /// <summary>
+        /// Adds the exit cells to the cells collection being built.
+        /// </summary>
+        /// <param name="positions">the position where the exits must be placed</param>
+        void AddExits(ISet<IPosition> positions);
+
+        /// <summary>
+        /// Adds the slider cells to the cells collection being built.
+        /// </summary>
+        /// <param name="positions">the position where the sliders must be placed</param>
+        void AddSliders(ISet<IPosition> positions);
+
+		/// <summary>
+		/// Adds the basic cells to the cells collection being built.
+		/// </summary>
+		void AddBasicCells();
+
+		/// <returns>the collection of cells that has been set up</returns>
+		IDictionary<IPosition, ICell> Build();
+    }
+}
