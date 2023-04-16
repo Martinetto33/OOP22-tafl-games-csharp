9876c1edaec8822498cf7a11a60a02ced2690c9f
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-04-16 13:09:14 +0200

    feat: add player implementation

---
+++ Boschetti/TaflGames/TaflGamesMatchSetup/Common/Player.cs
@@ -0,0 +1,17 @@
+﻿namespace Common
+{
+	/// <summary>
+	/// Represents the role of the player during the match.
+	/// </summary>
+	public enum Player
+	{
+		/// <summary>
+		/// The player that owns the attacking team.
+		/// </summary>
+		Attacker,
+		/// <summary>
+		/// The player that owns the defending team.
+		/// </summary>
+		Defender
+	}
+}
