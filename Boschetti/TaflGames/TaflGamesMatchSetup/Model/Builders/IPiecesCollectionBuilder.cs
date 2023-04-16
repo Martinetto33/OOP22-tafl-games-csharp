48caaa01ebbf30383933596717a32c1ddb5f2530
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-04-16 13:10:53 +0200

    feat: add pieces collection builder

---
+++ Boschetti/TaflGames/TaflGamesMatchSetup/Model/Builders/IPiecesCollectionBuilder.cs
@@ -0,0 +1,50 @@
+﻿using Common;
+using Pieces;
+
+namespace Builders
+{
+    /// <summary>
+    /// This interface allows to interact with a builder to create a collection of pieces.
+    /// </summary>
+	public interface IPiecesCollectionBuilder
+	{
+        /// <summary>
+        /// Adds the king piece to the pieces collection being built.
+        /// </summary>
+        /// <param name="position">the position where the king must be placed</param>
+		void AddKing(IPosition position);
+
+        /// <summary>
+        /// Adds the queen pieces to the pieces collection being built.
+        /// </summary>
+        /// <param name="positions">the position where the queens must be placed</param>
+		void AddQueens(IDictionary<Player, ISet<IPosition>> positions);
+
+        /// <summary>
+        /// Adds the archer pieces to the pieces collection being built.
+        /// </summary>
+        /// <param name="positions">the position where the archers must be placed</param>
+        void AddArchers(IDictionary<Player, ISet<IPosition>> positions);
+
+        /// <summary>
+        /// Adds the shield pieces to the pieces collection being built.
+        /// </summary>
+        /// <param name="positions">the position where the shields must be placed</param>
+        void AddShields(IDictionary<Player, ISet<IPosition>> positions);
+
+        /// <summary>
+        /// Adds the swapper pieces to the pieces collection being built.
+        /// </summary>
+        /// <param name="positions">the position where the swappers must be placed</param>
+        void AddSwappers(IDictionary<Player, ISet<IPosition>> positions);
+
+        /// <summary>
+        /// Adds the basic pieces to the pieces collection being built.
+        /// </summary>
+        /// <param name="positions">the position where the basic pieces must be placed</param>
+        void AddBasicPieces(IDictionary<Player, ISet<IPosition>> positions);
+
+        /// <returns>the collection of pieces that has been set up</returns>
+        IDictionary<Player, IDictionary<IPosition, IPiece>> Build();
+    }
+}
---
+++ Boschetti/TaflGames/TaflGamesMatchSetup/Model/Builders/PiecesCollectionBuilder.cs
@@ -0,0 +1,72 @@
+﻿using Common;
+using Pieces;
+
+namespace Builders
+{
+    /// <summary>
+    /// This class implements a builder to create a collection of pieces.
+    /// </summary>
+    public class PiecesCollectionBuilder : IPiecesCollectionBuilder
+    {
+        private readonly IDictionary<Player, IDictionary<IPosition, IPiece>> _pieces;
+
+        /// <summary>
+        /// Creates a new builder for a collection of pieces.
+        /// </summary>
+        public PiecesCollectionBuilder()
+        {
+            _pieces = new Dictionary<Player, IDictionary<IPosition, IPiece>>
+            {
+                { Player.Attacker, new Dictionary<IPosition, IPiece>() },
+                { Player.Defender, new Dictionary<IPosition, IPiece>() }
+            };
+        }
+
+        public void AddKing(IPosition position) => _pieces[Player.Defender].Add(position, new King());
+
+        public void AddQueens(IDictionary<Player, ISet<IPosition>> positions)
+        {
+            foreach (var (player, playerPositions) in positions)
+            {
+                playerPositions.ToList().ForEach(pos => _pieces[player].Add(pos, new Queen()));
+            }
+        }
+
+        public void AddArchers(IDictionary<Player, ISet<IPosition>> positions)
+        {
+            foreach (var (player, playerPositions) in positions)
+            {
+                playerPositions.ToList().ForEach(pos => _pieces[player].Add(pos, new Archer()));
+            }
+        }
+
+        public void AddBasicPieces(IDictionary<Player, ISet<IPosition>> positions)
+        {
+            foreach (var (player, playerPositions) in positions)
+            {
+                playerPositions.ToList().ForEach(pos => _pieces[player].Add(pos, new BasicPiece()));
+            }
+        }
+
+        public void AddShields(IDictionary<Player, ISet<IPosition>> positions)
+        {
+            foreach (var (player, playerPositions) in positions)
+            {
+                playerPositions.ToList().ForEach(pos => _pieces[player].Add(pos, new Shield()));
+            }
+        }
+
+        public void AddSwappers(IDictionary<Player, ISet<IPosition>> positions)
+        {
+            foreach (var (player, playerPositions) in positions)
+            {
+                playerPositions.ToList().ForEach(pos => _pieces[player].Add(pos, new Swapper()));
+            }
+        }
+
+        public IDictionary<Player, IDictionary<IPosition, IPiece>> Build()
+        {
+            return new Dictionary<Player, IDictionary<IPosition, IPiece>>(_pieces);
+        }
+    }
+}
