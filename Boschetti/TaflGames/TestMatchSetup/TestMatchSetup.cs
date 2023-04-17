f68a66ff0806f9735d0874a5d000fb28390058e2
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-04-17 19:08:57 +0200

    test: add test project

---
+++ Boschetti/TaflGames/TestMatchSetup/TestMatchSetup.cs
@@ -0,0 +1,16 @@
+namespace TestMatchSetup
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
