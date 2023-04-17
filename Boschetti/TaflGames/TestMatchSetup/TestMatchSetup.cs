6d4b4a0fd81daba415b3bca69ebefe3b1a9212ca
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-04-17 21:09:21 +0200

    test: add test initialization and add test for completion of settings loading from file

--- Boschetti/TaflGames/TestMatchSetup/TestMatchSetup.cs
+++ Boschetti/TaflGames/TestMatchSetup/TestMatchSetup.cs
@@ -1,16 +1,39 @@
-namespace TestMatchSetup
+using Builders;
+using NUnit.Framework.Internal;
+using SettingsLoaders;
+
+namespace TaflGames.TestMatchSetup
 {
     public class Tests
     {
+        private const int BoardSize = 11;
+
+        private ICellsCollectionBuilder _cellsCollBuilder;
+        private IPiecesCollectionBuilder _piecesCollBuilder;
+
         [SetUp]
         public void Setup()
         {
+            _cellsCollBuilder = new CellsCollectionBuilder();
+            _piecesCollBuilder = new PiecesCollectionBuilder();
         }
 
+        /// <summary>
+        /// Test the match setup according to the classic mode settings.
+        /// </summary>
         [Test]
-        public void Test1()
+        public void TestClassicModeMatchSetup()
         {
-            Assert.Pass();
+            ISettingsLoader loader = new SettingsLoader();
+            try
+            {
+                loader.LoadClassicModeConfig(_cellsCollBuilder, _piecesCollBuilder);
+            }
+            catch (IOException ex)
+            {
+                Assert.Fail("Error: could not read configuration file. " + ex.ToString());
+            }
+
         }
     }
 }
